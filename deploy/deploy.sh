264f52c9595f721c67a14ad373ef887ea05c29f2
Anand Gupta <anand@anand.com>
2025-04-19 15:33:23 +0530

    added working directory field in work flow

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,5 @@
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-pwd
 cd ../argocd-pipeline/
-pwd
 kubectl apply -f argo-cd-app.yaml
