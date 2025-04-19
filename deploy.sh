662335534526c8600fa3f69633d8d4cbecb88bd6
Anand Gupta <anand@anand.com>
2025-04-19 11:53:31 +0530

    adding the config file for k8s

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 kubectl apply -f argocd-pipeline/argo-cd-app.yaml
